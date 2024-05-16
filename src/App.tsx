ba5f60a7b5d50fb3605343cc1055be7030c1e56e
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-05-16 11:13:01 +0600

    page and components folder create ,create page and setup app.tsx route with lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {lazy, Suspense} from "react";
+import Loader from "./components/Loader.tsx";
+
+const Home = lazy(() => import("./pages/Home.tsx"));
+const Search = lazy(() => import("./pages/Search.tsx"));
+const Cart = lazy(() => import("./pages/Cart.tsx"));
 
 
 const App = () => {
     return (
-        <div>App</div>
+        <Router>
+            {/*  Header   */}
+            <Suspense fallback={<Loader/>}>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/search" element={<Search/>}/>
+                    <Route path="/cart" element={<Cart/>}/>
+                </Routes>
+            </Suspense>
+        </Router>
     )
 }
 export default App

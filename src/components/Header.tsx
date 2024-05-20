d1138421c9d592ac165c402aac03f01d637eddcb
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-05-21 00:56:54 +0600

    create login.jsx for user login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,47 +3,57 @@ import {FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser} from "react-
 import {useState} from "react";
 
 const user = {
-    _id: "",
-    role: "admin"
-}
-
+	_id: "",
+	role: "",
+};
 
 const Header = () => {
-    const [isOpen, setIsOpen] = useState<boolean>(true)
+	const [isOpen, setIsOpen] = useState<boolean>(true);
 
-    const handleLogout = ()=> {
-        setIsOpen(false)
-        console.log("Logout")
-    }
+	const handleLogout = () => {
+		setIsOpen(false);
+		console.log("Logout");
+	};
 
+	return (
+		<nav className='header'>
+			<Link to={"/"} onClick={() => setIsOpen(false)}>
+				HOME
+			</Link>
+			<Link to={"/search"} onClick={() => setIsOpen(false)}>
+				<FaSearch />
+			</Link>
+			<Link to={"/cart"} onClick={() => setIsOpen(false)}>
+				<FaShoppingBag />
+			</Link>
+			{user && user?._id ? (
+				<>
+					<button onClick={() => setIsOpen((prev) => !prev)} type='button'>
+						<FaUser />
+					</button>
+					<dialog open={isOpen}>
+						<div>
+							{user?.role === "admin" && (
+								<Link to={"/admin/dashboard"} onClick={() => setIsOpen(false)}>
+									Admin
+								</Link>
+							)}
 
-    return (
-        <nav className="header">
-            <Link to={"/"} onClick={()=> setIsOpen(false)} >HOME</Link>
-            <Link to={"/search"} onClick={()=> setIsOpen(false)}><FaSearch/></Link>
-            <Link to={"/cart"} onClick={()=> setIsOpen(false)}><FaShoppingBag/></Link>
-            {
-                user && user?._id ? (
-                    <>
-                        <button onClick={() => setIsOpen((prev) => !prev)} type="button"><FaUser/></button>
-                        <dialog open={isOpen}>
-                            <div>
-                                {
-                                    user?.role === "admin" && (
-                                        <Link to={"/admin/dashboard"} onClick={()=> setIsOpen(false)} >Admin</Link>
-                                    )
-                                }
-
-                                <Link to={"/orders"} onClick={()=> setIsOpen(false)} >Orders</Link>
-                                <button onClick={handleLogout} type="button"><FaSignOutAlt/></button>
-                            </div>
-                        </dialog>
-                    </>
-                ) : (
-                    <Link to={"/login"}><FaSignInAlt/></Link>
-                )
-            }
-        </nav>
-    )
-}
-export default Header
+							<Link to={"/orders"} onClick={() => setIsOpen(false)}>
+								Orders
+							</Link>
+							<button onClick={handleLogout} type='button'>
+								<FaSignOutAlt />
+							</button>
+						</div>
+					</dialog>
+				</>
+			) : (
+				<Link to={"/login"}>
+					<FaSignInAlt />
+				</Link>
+			)}
+		</nav>
+	);
+};
+export default Header;

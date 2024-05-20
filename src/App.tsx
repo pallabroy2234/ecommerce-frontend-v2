aa7ecfe476c04e74a3b2341763fecc9182829d04
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-05-21 00:51:06 +0600

    shipping page design complete

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import {lazy, Suspense} from "react";
 import Loader from "./components/Loader.tsx";
 import Header from "./components/Header.tsx";
 
-
 const Home = lazy(() => import("./pages/Home.tsx"));
 const Search = lazy(() => import("./pages/Search.tsx"));
 const Cart = lazy(() => import("./pages/Cart.tsx"));
@@ -21,58 +20,55 @@ const Coupon = lazy(() => import("./pages/admin/apps/coupon"));
 const Stopwatch = lazy(() => import("./pages/admin/apps/stopwatch"));
 const Toss = lazy(() => import("./pages/admin/apps/toss"));
 const NewProduct = lazy(() => import("./pages/admin/management/newproduct"));
-const ProductManagement = lazy(
-    () => import("./pages/admin/management/productmanagement")
-);
-const TransactionManagement = lazy(
-    () => import("./pages/admin/management/transactionmanagement")
-);
-
+const ProductManagement = lazy(() => import("./pages/admin/management/productmanagement"));
+const TransactionManagement = lazy(() => import("./pages/admin/management/transactionmanagement"));
 
 const App = () => {
-    return (
-        <Router>
-            {/*  Header   */}
+	return (
+		<Router>
+			{/*  Header   */}
 
-            <Header/>
+			<Header />
 
-            <Suspense fallback={<Loader/>}>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/search" element={<Search/>}/>
-                    <Route path="/cart" element={<Cart/>}/>
+			<Suspense fallback={<Loader />}>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/search' element={<Search />} />
+					<Route path='/cart' element={<Cart />} />
 
-                    {/* Logged in user routes */}
-                    <Route path="/shipping" element={<Shipping/>}/>
-                    {/*  admin routes  */}
-                    <Route
-                        // element={
-                        //     <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
-                        // }
-                    >
-                        <Route path="/admin/dashboard" element={<Dashboard/>}/>
-                        <Route path="/admin/product" element={<Products/>}/>
-                        <Route path="/admin/customer" element={<Customers/>}/>
-                        <Route path="/admin/transaction" element={<Transaction/>}/>
-                        {/* Charts */}
-                        <Route path="/admin/chart/bar" element={<Barcharts/>}/>
-                        <Route path="/admin/chart/pie" element={<Piecharts/>}/>
-                        <Route path="/admin/chart/line" element={<Linecharts/>}/>
-                        {/* Apps */}
-                        <Route path="/admin/app/coupon" element={<Coupon/>}/>
-                        <Route path="/admin/app/stopwatch" element={<Stopwatch/>}/>
-                        <Route path="/admin/app/toss" element={<Toss/>}/>
+					{/* Logged in user routes */}
+					<Route>
+						<Route path='/shipping' element={<Shipping />} />
+					</Route>
+					{/*  admin routes  */}
+					<Route
+					// element={
+					//     <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
+					// }
+					>
+						<Route path='/admin/dashboard' element={<Dashboard />} />
+						<Route path='/admin/product' element={<Products />} />
+						<Route path='/admin/customer' element={<Customers />} />
+						<Route path='/admin/transaction' element={<Transaction />} />
+						{/* Charts */}
+						<Route path='/admin/chart/bar' element={<Barcharts />} />
+						<Route path='/admin/chart/pie' element={<Piecharts />} />
+						<Route path='/admin/chart/line' element={<Linecharts />} />
+						{/* Apps */}
+						<Route path='/admin/app/coupon' element={<Coupon />} />
+						<Route path='/admin/app/stopwatch' element={<Stopwatch />} />
+						<Route path='/admin/app/toss' element={<Toss />} />
 
-                        {/* Management */}
-                        <Route path="/admin/product/new" element={<NewProduct/>}/>
+						{/* Management */}
+						<Route path='/admin/product/new' element={<NewProduct />} />
 
-                        <Route path="/admin/product/:id" element={<ProductManagement/>}/>
+						<Route path='/admin/product/:id' element={<ProductManagement />} />
 
-                        <Route path="/admin/transaction/:id" element={<TransactionManagement/>}/>
-                    </Route>
-                </Routes>
-            </Suspense>
-        </Router>
-    )
-}
-export default App
+						<Route path='/admin/transaction/:id' element={<TransactionManagement />} />
+					</Route>
+				</Routes>
+			</Suspense>
+		</Router>
+	);
+};
+export default App;
--- src/pages/Shipping.tsx
+++ src/pages/Shipping.tsx
@@ -1,8 +1,47 @@
+import {ChangeEvent, useState} from "react";
+import {BiArrowBack} from "react-icons/bi";
+import {useNavigate} from "react-router-dom";
 
+type ShippingInfo = {
+	address: string;
+	city: string;
+	state: string;
+	country: string;
+	pinCode: string;
+};
 
 const Shipping = () => {
-    return (
-        <div>Shipping</div>
-    )
-}
-export default Shipping
+	const navigate = useNavigate();
+	const [shippingInfo, setShippingInfo] = useState<ShippingInfo>({
+		address: "",
+		city: "",
+		state: "",
+		country: "",
+		pinCode: "",
+	});
+
+	const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+		setShippingInfo((prev) => ({...prev, [e.target.name]: e.target.value}));
+	};
+
+	return (
+		<div className='shipping'>
+			<button onClick={() => navigate("/cart")} className='back-btn'>
+				<BiArrowBack />
+			</button>
+			<form>
+				<h1>Shipping Address</h1>
+				<input onChange={(e) => handleChange(e)} required type='text' name='address' value={shippingInfo.address} placeholder='Address' />
+				<input onChange={(e) => handleChange(e)} required type='text' name='city' value={shippingInfo.city} placeholder='City' />
+				<input onChange={(e) => handleChange(e)} required type='text' name='state' value={shippingInfo.state} placeholder='state' />
+				<select onChange={(e) => handleChange(e)} name='country' required value={shippingInfo.country}>
+					<option value=''>Choose Country</option>
+					<option value='Bangladesh'>Bangladesh</option>
+				</select>
+				<input onChange={(e) => handleChange(e)} required type='number' name='pinCode' value={shippingInfo.pinCode} placeholder='Pin Code' />
+				<button type='submit'>Pay Now</button>
+			</form>
+		</div>
+	);
+};
+export default Shipping;

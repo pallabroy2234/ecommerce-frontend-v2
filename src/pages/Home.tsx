5f674d7fa3f079d7379902ed92a710b2497614dc
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-08-15 21:47:07 +0600

    install: install redux-toolkit react-redux axios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,25 @@
 import {Link} from "react-router-dom";
 import ProductCard from "../components/ProductCard.tsx";
 
-
 const Home = () => {
-    const handleAddToCart  = () => {
-        console.log("Added to Cart")
-    }
-    return (
-        <div className="home">
-             <section></section>
-            <h1>Latest Product <Link to={"/search"} className="findmore" >More</Link> </h1>
-            <main>
-                <ProductCard productId="asdfasd"  image="https://m.media-amazon.com/images/I/81dVkD9YyOL._AC_SX522_.jpg" name="Apple Macbook" price={1000} stock={20} handler={handleAddToCart}/>
-                <ProductCard productId="asdfasd"  image="https://m.media-amazon.com/images/I/81dVkD9YyOL._AC_SX522_.jpg" name="Apple Macbook" price={1000} stock={20} handler={handleAddToCart}/>
-                <ProductCard productId="asdfasd"  image="https://m.media-amazon.com/images/I/81dVkD9YyOL._AC_SX522_.jpg" name="Apple Macbook" price={1000} stock={20} handler={handleAddToCart}/>
-            </main>
-        </div>
-    )
-}
-export default Home
+	const handleAddToCart = () => {
+		console.log("Added to Cart");
+	};
+	return (
+		<div className='home'>
+			<section></section>
+			<h1>
+				Latest Product{" "}
+				<Link to={"/search"} className='findmore'>
+					More
+				</Link>{" "}
+			</h1>
+			<main>
+				<ProductCard productId='asdfasd' image='https://m.media-amazon.com/images/I/81dVkD9YyOL._AC_SX522_.jpg' name='Apple Macbook' price={1000} stock={20} handler={handleAddToCart} />
+				<ProductCard productId='asdfasd' image='https://m.media-amazon.com/images/I/81dVkD9YyOL._AC_SX522_.jpg' name='Apple Macbook' price={1000} stock={20} handler={handleAddToCart} />
+				<ProductCard productId='asdfasd' image='https://m.media-amazon.com/images/I/81dVkD9YyOL._AC_SX522_.jpg' name='Apple Macbook' price={1000} stock={20} handler={handleAddToCart} />
+			</main>
+		</div>
+	);
+};
+export default Home;

e63497ea22438e761569eeac8d825e567db6a745
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-08-27 16:03:30 +0600

    feat: complete if has no cart item than not entry shipping route functionality

--- src/pages/Shipping.tsx
+++ src/pages/Shipping.tsx
@@ -1,6 +1,8 @@
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, useEffect, useState} from "react";
 import {BiArrowBack} from "react-icons/bi";
 import {useNavigate} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {CartReducerInitialState} from "../types/reducer-types.ts";
 
 type ShippingInfo = {
 	address: string;
@@ -11,7 +13,11 @@ type ShippingInfo = {
 };
 
 const Shipping = () => {
+	const {cartItems} = useSelector(
+		(state: {cartReducer: CartReducerInitialState}) => state.cartReducer,
+	);
 	const navigate = useNavigate();
+
 	const [shippingInfo, setShippingInfo] = useState<ShippingInfo>({
 		address: "",
 		city: "",
@@ -24,6 +30,12 @@ const Shipping = () => {
 		setShippingInfo((prev) => ({...prev, [e.target.name]: e.target.value}));
 	};
 
+	useEffect(() => {
+		if (cartItems.length <= 0) {
+			navigate("/cart");
+		}
+	}, [cartItems]);
+
 	return (
 		<div className='shipping'>
 			<button onClick={() => navigate("/cart")} className='back-btn'>
@@ -31,14 +43,46 @@ const Shipping = () => {
 			</button>
 			<form>
 				<h1>Shipping Address</h1>
-				<input onChange={(e) => handleChange(e)} required type='text' name='address' value={shippingInfo.address} placeholder='Address' />
-				<input onChange={(e) => handleChange(e)} required type='text' name='city' value={shippingInfo.city} placeholder='City' />
-				<input onChange={(e) => handleChange(e)} required type='text' name='state' value={shippingInfo.state} placeholder='State' />
-				<select onChange={(e) => handleChange(e)} name='country' required value={shippingInfo.country}>
+				<input
+					onChange={(e) => handleChange(e)}
+					required
+					type='text'
+					name='address'
+					value={shippingInfo.address}
+					placeholder='Address'
+				/>
+				<input
+					onChange={(e) => handleChange(e)}
+					required
+					type='text'
+					name='city'
+					value={shippingInfo.city}
+					placeholder='City'
+				/>
+				<input
+					onChange={(e) => handleChange(e)}
+					required
+					type='text'
+					name='state'
+					value={shippingInfo.state}
+					placeholder='State'
+				/>
+				<select
+					onChange={(e) => handleChange(e)}
+					name='country'
+					required
+					value={shippingInfo.country}>
 					<option value=''>Choose Country</option>
 					<option value='Bangladesh'>Bangladesh</option>
 				</select>
-				<input onChange={(e) => handleChange(e)} required type='number' name='pinCode' value={shippingInfo.pinCode} placeholder='Pin Code' />
+				<input
+					onChange={(e) => handleChange(e)}
+					required
+					type='number'
+					name='pinCode'
+					value={shippingInfo.pinCode}
+					placeholder='Pin Code'
+				/>
 				<button type='submit'>Pay Now</button>
 			</form>
 		</div>

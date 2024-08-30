22f04ed394b94c3cc982d2621bf298cd87fc94c9
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-08-30 18:34:50 +0600

    feat(CheckOut.tsx): stripe confirm payment config

--- src/pages/CheckOut.tsx
+++ src/pages/CheckOut.tsx
@@ -1,21 +1,70 @@
-import {Elements} from "@stripe/react-stripe-js";
+import {Elements, PaymentElement, useElements, useStripe} from "@stripe/react-stripe-js";
 import {loadStripe} from "@stripe/stripe-js";
+import {FormEvent, useState} from "react";
+import toast from "react-hot-toast";
+import {Navigate, useLocation, useNavigate} from "react-router-dom";
+import {frontendUrl} from "../redux/api/apiBaseUrl.ts";
 
 const stripePromise = loadStripe(
 	"pk_test_51P85zqKkIuFqly0WcH0jii0JelERIPZjhKbj0NxNftJhxfKF5ZntMxTbVP60csDXya2E0tBYAv54YFzxRRSG4mFe00ejlUU33F",
 );
 
 const CheckOutForm = () => {
+	const stripe = useStripe();
+	const elements = useElements();
+	const navigate = useNavigate();
+	const [isProcessing, setIsProcessing] = useState<boolean>(false);
+
+	const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (!stripe || !elements) return;
+		setIsProcessing(true);
+
+		// const order = {};
+
+		// * Confirm the payment
+		const {paymentIntent, error} = await stripe.confirmPayment({
+			elements,
+			confirmParams: {
+				return_url: frontendUrl,
+			},
+			redirect: "if_required",
+		});
+
+		if (error) {
+			setIsProcessing(false);
+			toast.error(error.message || "An error occurred while processing the payment");
+		}
+		if (paymentIntent?.status === "succeeded") {
+			navigate("/orders");
+		}
+		setIsProcessing(false);
+	};
+
 	return (
-		<div>
-			<h1>CheckOut</h1>
+		<div className='checkout-container'>
+			<form onSubmit={submitHandler}>
+				<PaymentElement />
+				<button type='submit' disabled={isProcessing}>
+					{isProcessing ? "Processing..." : "Pay"}
+				</button>
+			</form>
 		</div>
 	);
 };
 
 const CheckOut = () => {
+	const location = useLocation();
+	const clientSecret: string = location.state || undefined;
+
+	if (!clientSecret) return <Navigate to={"/shipping"} />;
+
 	return (
-		<Elements stripe={stripePromise}>
+		<Elements
+			stripe={stripePromise}
+			options={{
+				clientSecret: clientSecret,
+			}}>
 			<CheckOutForm />
 		</Elements>
 	);
--- src/redux/api/apiBaseUrl.ts
+++ src/redux/api/apiBaseUrl.ts
@@ -14,3 +14,20 @@ const getApiBaseUrl = (): string => {
 };
 
 export const apiBaseUrl: string = getApiBaseUrl();
+
+const getFrontendUrl = (): string => {
+	switch (import.meta.env.VITE_NODE_ENV) {
+		case "development":
+			return import.meta.env.VITE_DEVELOPMENT_FRONTEND_URL as string;
+		case "production":
+			return import.meta.env.VITE_PRODUCTION_FRONTEND_URL as string;
+		case "testing":
+			return import.meta.env.VITE_TESTING_FRONTEND_URL as string;
+		case "staging":
+			return import.meta.env.VITE_STAGING_FRONTEND_URL as string;
+		default:
+			throw new Error("Invalid NODE_ENV value");
+	}
+};
+
+export const frontendUrl: string = getFrontendUrl();

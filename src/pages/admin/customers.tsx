a964302f5cc9c754217cf118997acd08b65fe111
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-09-04 02:14:47 +0600

    featt(customer.tsx): complete data implemantation and also delete user

--- src/pages/admin/customers.tsx
+++ src/pages/admin/customers.tsx
@@ -1,109 +1,113 @@
-import { ReactElement, useState } from "react";
-import { FaTrash } from "react-icons/fa";
-import { Column } from "react-table";
+import {ReactElement, useEffect, useState} from "react";
+import {Column} from "react-table";
 import AdminSidebar from "../../components/admin/AdminSidebar";
 import TableHOC from "../../components/admin/TableHOC";
+import {useSelector} from "react-redux";
+import {RootState} from "../../redux/store.ts";
+import {useDeleteUserByAdminMutation, useGetAllUserByAdminQuery} from "../../redux/api/userAPI.ts";
+import {CustomError} from "../../types/api-types.ts";
+import toast from "react-hot-toast";
+import {Skeleton} from "../../components/Loader.tsx";
+import {FaTrash} from "react-icons/fa";
+import {responseToast} from "../../utils/feature.ts";
 
 interface DataType {
-  avatar: ReactElement;
-  name: string;
-  email: string;
-  gender: string;
-  role: string;
-  action: ReactElement;
+	avatar: ReactElement;
+	name: string;
+	email: string;
+	gender: string;
+	role: string;
+	action: ReactElement;
 }
 
 const columns: Column<DataType>[] = [
-  {
-    Header: "Avatar",
-    accessor: "avatar",
-  },
-  {
-    Header: "Name",
-    accessor: "name",
-  },
-  {
-    Header: "Gender",
-    accessor: "gender",
-  },
-  {
-    Header: "Email",
-    accessor: "email",
-  },
-  {
-    Header: "Role",
-    accessor: "role",
-  },
-  {
-    Header: "Action",
-    accessor: "action",
-  },
-];
-
-const img = "https://randomuser.me/api/portraits/women/54.jpg";
-const img2 = "https://randomuser.me/api/portraits/women/50.jpg";
-
-const arr: Array<DataType> = [
-  {
-    avatar: (
-      <img
-        style={{
-          borderRadius: "50%",
-        }}
-        src={img}
-        alt="Shoes"
-      />
-    ),
-    name: "Emily Palmer",
-    email: "emily.palmer@example.com",
-    gender: "female",
-    role: "user",
-    action: (
-      <button>
-        <FaTrash />
-      </button>
-    ),
-  },
-
-  {
-    avatar: (
-      <img
-        style={{
-          borderRadius: "50%",
-        }}
-        src={img2}
-        alt="Shoes"
-      />
-    ),
-    name: "May Scoot",
-    email: "aunt.may@example.com",
-    gender: "female",
-    role: "user",
-    action: (
-      <button>
-        <FaTrash />
-      </button>
-    ),
-  },
+	{
+		Header: "Avatar",
+		accessor: "avatar",
+	},
+	{
+		Header: "Name",
+		accessor: "name",
+	},
+	{
+		Header: "Gender",
+		accessor: "gender",
+	},
+	{
+		Header: "Email",
+		accessor: "email",
+	},
+	{
+		Header: "Role",
+		accessor: "role",
+	},
+	{
+		Header: "Action",
+		accessor: "action",
+	},
 ];
 
 const Customers = () => {
-  const [rows, setRows] = useState<DataType[]>(arr);
+	const {user} = useSelector((state: RootState) => state.userReducer);
+	const [rows, setRows] = useState<DataType[]>([]);
+	const {data, isLoading, isError, error} = useGetAllUserByAdminQuery(user?._id || "");
+	// Get all users
+	const users = data?.payload || [];
+	//  Delete user mutation
+	const [deleteUser] = useDeleteUserByAdminMutation();
 
-  const Table = TableHOC<DataType>(
-    columns,
-    rows,
-    "dashboard-product-box",
-    "Customers",
-    rows.length > 6
-  )();
+	const Table = TableHOC<DataType>(
+		columns,
+		rows,
+		"dashboard-product-box",
+		"Customers",
+		rows.length > 6,
+	)();
 
-  return (
-    <div className="admin-container">
-      <AdminSidebar />
-      <main>{Table}</main>
-    </div>
-  );
+	// Delete handler
+	const handleDeleteUser = async (id: string) => {
+		try {
+			const res = await deleteUser({userId: id, id: user?._id || ""});
+			responseToast(res, null, "");
+		} catch (error) {
+			toast.error("Something went wrong");
+		}
+	};
+
+	// Data
+	useEffect(() => {
+		if (data) {
+			setRows(
+				users.map((item) => ({
+					avatar: <img src={`${item?.image}`} alt={item?.name} />,
+					name: item?.name,
+					email: item?.email,
+					gender: item?.gender,
+					role: item?.role!,
+					action: (
+						<button type='button' onClick={() => handleDeleteUser(item?._id)}>
+							<FaTrash />
+						</button>
+					),
+				})),
+			);
+		}
+	}, [data]);
+
+	// handle Error
+	useEffect(() => {
+		if (isError) {
+			const err = error as CustomError;
+			toast.error(err.data.message);
+		}
+	}, [isError, error]);
+
+	return (
+		<div className='admin-container'>
+			<AdminSidebar />
+			<main>{isLoading ? <Skeleton length={20} /> : Table}</main>
+		</div>
+	);
 };
 
 export default Customers;

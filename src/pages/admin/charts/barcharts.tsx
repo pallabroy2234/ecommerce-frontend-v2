a04fd27ade28543d20b03d9a1a28b3c422e80d9c
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-09-08 17:53:09 +0600

    feat(barcharts.tsx): complete implemantation barCharts Data

--- src/pages/admin/charts/barcharts.tsx
+++ src/pages/admin/charts/barcharts.tsx
@@ -7,6 +7,7 @@ import {useEffect} from "react";
 import {CustomError} from "../../../types/api-types.ts";
 import toast from "react-hot-toast";
 import {BarChart as BarChartType} from "../../../types/types.ts";
+import {Skeleton} from "../../../components/Loader.tsx";
 
 const months = [
 	"January",
@@ -47,31 +48,37 @@ const Barcharts = () => {
 			<AdminSidebar />
 			<main className='chart-container'>
 				<h1>Bar Charts</h1>
-				<section>
-					<BarChart
-						data_2={[300, 144, 433, 655, 237, 755, 190]}
-						data_1={[200, 444, 343, 556, 778, 455, 990]}
-						title_1='Products'
-						title_2='Users'
-						bgColor_1={`hsl(260, 50%, 30%)`}
-						bgColor_2={`hsl(360, 90%, 90%)`}
-					/>
-					<h2>Top Products & Top Customers</h2>
-				</section>
+				{isLoading ? (
+					<Skeleton length={20} />
+				) : (
+					<>
+						<section>
+							<BarChart
+								data_2={productCount}
+								data_1={userCount}
+								title_1='Products'
+								title_2='Users'
+								bgColor_1={`hsl(260, 50%, 30%)`}
+								bgColor_2={`hsl(360, 90%, 90%)`}
+							/>
+							<h2>Top Products & Top Customers</h2>
+						</section>
 
-				<section>
-					<BarChart
-						horizontal={true}
-						data_1={[200, 444, 343, 556, 778, 455, 990, 444, 122, 334, 890, 909]}
-						data_2={[]}
-						title_1='Orders'
-						title_2=''
-						bgColor_1={`hsl(180, 40%, 50%)`}
-						bgColor_2=''
-						labels={months}
-					/>
-					<h2>Orders throughout the year</h2>
-				</section>
+						<section>
+							<BarChart
+								horizontal={true}
+								data_1={orderCount}
+								data_2={[]}
+								title_1='Orders'
+								title_2=''
+								bgColor_1={`hsl(180, 40%, 50%)`}
+								bgColor_2=''
+								labels={months}
+							/>
+							<h2>Orders throughout the year</h2>
+						</section>
+					</>
+				)}
 			</main>
 		</div>
 	);
